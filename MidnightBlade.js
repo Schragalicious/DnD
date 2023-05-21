75d319381a4bca20a9d85b4e9c3dbd4fb9ab1a88
Schragalicious <134228976+Schragalicious@users.noreply.github.com>
2023-05-21 15:09:18 -0400

    Create MidnightBlade.js
    Proper Midnight Blade Java File

---
+++ MidnightBlade.js
@@ -0,0 +1,10 @@
+var iFileName = "Homebrew Syntax - MagicItemsList.js";
+MagicItemsList["Midnight Blade"] = {
+name : "Midnight Blade",
+source : ["HB", 0],
+type : "weapon",
+rarity : "legendary",
+attunement : true,
+weight : 7,
+description : "I have a +3 to hit with this magical katana. On a Nat 20 roll, the blade slices through any object, halving enemy health. I can decide to use the great slice.",
+descriptionLong : "A deep, navy-blue blade surging with demonic power."}
