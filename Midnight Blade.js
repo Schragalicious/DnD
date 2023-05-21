6816820b4f62aedbdbb6fa22d3fb33029c217f88
Schragalicious <134228976+Schragalicious@users.noreply.github.com>
2023-05-21 15:21:20 -0400

    Weapon: Midnight Blade.js

---
+++ Midnight Blade.js
@@ -0,0 +1,14 @@
+var iFileName = "Homebrew Syntax - MagicItemsList.js";
+WeaponsList["Midnight Blade"] = {
+name : "Midnight Blade",
+source : ["HB", 0],
+regExpSearch : /^(?=.*Midnight)(?=.*Blade).*$/i,
+type: "Martial",
+ability : 2,
+abilitytodamage : true,
+damage : [1, 8, "slashing"],
+range : "Melee",
+description : "Finesse, light",
+modifiers : [prof+dex+3, ""],
+isMagicWeapon : true,
+}
