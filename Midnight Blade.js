51d60bd3651f80851abc5ab69864dbdf8130e105
Schragalicious <134228976+Schragalicious@users.noreply.github.com>
2023-05-23 22:19:40 -0400

    Weapon Entry Midnight Blade.js

--- Midnight Blade.js
+++ Midnight Blade.js
@@ -1,13 +1,14 @@
-var iFileName = "MidnightBlade.js";
 WeaponsList["Midnight Blade"] = {
 name : "Midnight Blade",
-source : ["HB", 0],
-regExpSearch : /^(?=.*Midnight)(?=.*Blade).*$/i,
+source : ["HB", 1],
+regExpSearch : /Midnight Blade/i,
 type: "Martial",
 ability : 2,
 abilitytodamage : true,
 damage : [1, 8, "slashing"],
 range : "Melee",
 description : "Finesse, light",
+tooltip : "I have a +3 on attack rolls for this Magic Weapon. On a Nat 20 Roll, my slash cuts through anything, halving enemy HP. I can choose to use the slash",
+list : "melee",
 isMagicWeapon : true,
 }
