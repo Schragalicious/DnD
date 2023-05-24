8ab87a4445066fbf6b404ce19b077150e1d5f32d
Schragalicious <134228976+Schragalicious@users.noreply.github.com>
2023-05-23 23:04:07 -0400

    Update The Complete Midnight Blade.js

--- The Complete Midnight Blade.js
+++ The Complete Midnight Blade.js
@@ -13,3 +13,12 @@ list : "melee",
 modifiers : [0, "Prof"],
 isMagicWeapon : true,
 }
+MagicItemsList["Midnight Blade"] = {
+name : "Midnight Blade",
+source : ["HB", 0],
+type : "weapon",
+rarity : "legendary",
+attunement : true,
+weight : 7,
+description : "I have a +3 to hit with this magical katana. On a Nat 20 roll, the blade slices through any object, halving enemy health. I can decide to use the great slice.",
+descriptionLong : "A deep, navy-blue blade surging with demonic power."}
