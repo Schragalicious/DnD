212b1307284492d6085b06450edfb8c8086ef6e6
Schragalicious <134228976+Schragalicious@users.noreply.github.com>
2023-07-17 20:44:17 -0400

    Update Feller's Chakrams.js

--- Feller's Chakrams.js
+++ Feller's Chakrams.js
@@ -1,24 +1,24 @@
-WeaponsList["Chakram"] = {
-name : "Chakram",
+WeaponsList["Trim Rings"] = {
+name : "Trim Rings",
 source : ["HB", 1],
-regExpSearch : /Chakram/i,
+regExpSearch : /Trim Rings/i,
 type: "Martial",
 ability : 2,
 abilitytodamage : true,
 damage : [1, 6, "slashing"],
 range : "Melee, 20/60 ft",
 description : "Finesse, light",
-tooltip : "Chakrams return to me after they are thrown",
+tooltip : "Trim Rings return to me after they are thrown",
 list : "melee",
 modifiers : [0, "Prof"],
 isMagicWeapon : true,
 }
-MagicItemsList["Chakrams"] = {
-name : "Chakrams",
+MagicItemsList["Trim Rings"] = {
+name : "Trim Rings",
 source : ["HB", 0],
 type : "weapon",
 rarity : "Uncommon",
 attunement : false,
 weight : 1,
-description : "I add Dexterity to my To Hit. Chakrams return to me after they are thrown.",
+description : "I add Dexterity to my To Hit. Trim Rings return to me after they are thrown.",
 descriptionLong : "Chakrams made to return after cutting tree branches."}
