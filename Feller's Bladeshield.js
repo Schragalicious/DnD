e9659e9bf1048379ebf87aa7928c699034cf29a1
Schragalicious <134228976+Schragalicious@users.noreply.github.com>
2023-07-17 20:51:17 -0400

    Update Feller's Bladeshield and Trim Rings.js

--- Feller's Bladeshield.js
+++ Feller's Bladeshield.js
@@ -1,3 +1,28 @@
+WeaponsList["Trim Rings"] = {
+name : "Trim Rings",
+source : ["HB", 1],
+regExpSearch : /Trim Rings/i,
+type: "Martial",
+ability : 2,
+abilitytodamage : true,
+damage : [1, 6, "slashing"],
+range : "Melee, 20/60 ft",
+description : "Finesse, light",
+tooltip : "Trim Rings return to me after they are thrown",
+list : "melee",
+modifiers : [0, "Prof"],
+isMagicWeapon : true,
+}
+MagicItemsList["Trim Rings"] = {
+name : "Trim Rings",
+source : ["HB", 0],
+type : "weapon",
+rarity : "Uncommon",
+attunement : false,
+weight : 1,
+description : "I add Dexterity to my To Hit. Trim Rings return to me after they are thrown.",
+descriptionLong : "Chakrams made to return after cutting tree branches."}
+
 ArmourList["Bladeshield"] = {
 name : "Bladeshield",
 source : ["HB", 1],
