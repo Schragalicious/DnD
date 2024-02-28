66a5d3dfd10ccbdc51209ce6700bfec75dc1f88b
Schragalicious <134228976+Schragalicious@users.noreply.github.com>
2024-02-27 21:58:38 -0500

    Dante.js

--- Dante.js
+++ Dante.js
@@ -10,7 +10,7 @@ range : "Melee",
 description : "Martial, Versatile",
 tooltip : "I have a +2 on attack rolls for this Magic Weapon. his weapon has 7 charges. When you hit with an attack using this weapon, you can use a bonus action and expend a charge to deal an additional 3d8 force damage to the target, or you can expend 2 charges to force each other creature within 15 feet line of you to make a DC 15 Constitution saving throw. On a failed save, a creature takes 2d12 thunder damage. The weapon regains 1d6 + 1 expended charges daily at dawn.",
 list : "melee",
-modifiers : [2],
+modifiers : [2,"str+2"],
 isMagicWeapon : true,
 }
 MagicItemsList["Booming Hammer"] = {
