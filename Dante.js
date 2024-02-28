79c67f11d187c25830311c052ffadb38b5ab51fa
Schragalicious <134228976+Schragalicious@users.noreply.github.com>
2024-02-27 21:55:35 -0500

    Create Dante.js

---
+++ Dante.js
@@ -0,0 +1,24 @@
+WeaponsList["Booming Hammer"] = {
+name : "Booming Hammer",
+source : ["HB", 1],
+regExpSearch : /Booming Hammer/i,
+type: "Martial",
+ability : 1,
+abilitytodamage : true,
+damage : [1, 8, "bludgeoning"],
+range : "Melee",
+description : "Martial, Versatile",
+tooltip : "I have a +2 on attack rolls for this Magic Weapon. his weapon has 7 charges. When you hit with an attack using this weapon, you can use a bonus action and expend a charge to deal an additional 3d8 force damage to the target, or you can expend 2 charges to force each other creature within 15 feet line of you to make a DC 15 Constitution saving throw. On a failed save, a creature takes 2d12 thunder damage. The weapon regains 1d6 + 1 expended charges daily at dawn.",
+list : "melee",
+modifiers : [2],
+isMagicWeapon : true,
+}
+MagicItemsList["Booming Hammer"] = {
+name : "Booming Hammer",
+source : ["HB", 0],
+type : "weapon",
+rarity : "rare",
+attunement : true,
+weight : 7,
+description : "I have a +2 to hit with this magical hammer. I have a +2 on attack rolls for this Magic Weapon. his weapon has 7 charges. When you hit with an attack using this weapon, you can use a bonus action and expend a charge to deal an additional 3d8 force damage to the target, or you can expend 2 charges to force each other creature within 15 feet line of you to make a DC 15 Constitution saving throw. On a failed save, a creature takes 2d12 thunder damage",
+}
