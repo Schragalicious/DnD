a78e4549b1fcb91d1a626538698b002eceab39a6
Zachary Schrag <134228976+Schragalicious@users.noreply.github.com>
2025-08-30 11:30:11 -0400

    Update MidgardianBlade.js

--- MidgardianBlade.js
+++ MidgardianBlade.js
@@ -12,7 +12,7 @@ range : "Melee",
 description : "Finesse",
 tooltip : "The Midgardian Blade emits a radiant glow, shedding bright light in a 20-foot radius and dim light for an additional 20 feet when drawn. Divine Smite: Once per long rest, when the wielder hits a creature with an attack using the Holy Blade of Midgard, they can choose to channel divine energy into the strike, dealing an additional 4d8 radiant damage. The damage increases to 6d8 against undead and fiends. Divine Protection: Once per long rest, The Midgardian Blade can bestow divine protection onto the wielder. The wielder gains advantage on saving throws against spells and other magical effects for 1d6 rounds.",
 list : "melee",
-modifiers : [2,"2"],
+modifiers : [1,"1"],
 isMagicWeapon : true,
 }
 
