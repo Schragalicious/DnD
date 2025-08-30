6d6fb1860966fca468428c847667a310288ea4f1
Zachary Schrag <134228976+Schragalicious@users.noreply.github.com>
2025-08-30 11:07:31 -0400

    Update Karstag.js

--- Karstag.js
+++ Karstag.js
@@ -15,12 +15,12 @@ list : "melee",
 modifiers : [2,"2"],
 isMagicWeapon : true,
 }
-MagicItemsList["Booming Hammer"] = {
+MagicItemsList["Sword of Karstag"] = {
 name : "Booming Hammer",
 source : ["HB", 0],
 type : "weapon",
 rarity : "artifact",
 attunement : true,
-weight : 7,
-description : "I have a +2 to hit with this magical hammer. I have a +2 on attack rolls for this Magic Weapon. his weapon has 7 charges. When you hit with an attack using this weapon, you can use a bonus action and expend a charge to deal an additional 3d8 force damage to the target, or you can expend 2 charges to force each other creature within 15 feet line of you to make a DC 15 Constitution saving throw. On a failed save, a creature takes 2d12 thunder damage",
-}
+weight : 14,
+description : "You have a +2 on attack rolls for this Magic Weapon. This weapon has 3 charges. When you roll a critical hit, you can expend a charge to deal an additional damage die of damage. The weapon regains 1d3 expended charges at dawn. As a Bonus Action, You can activate the Sword of Karstag's Wrath. For 1 minute, the sword emits an aura of power, shedding dim light in a 10-foot radius. While the aura is active, you gain the following benefits: Whenever you hit a creature with an attack using the sword, they must make a DC 18 Constitution saving throw or be stunned until the start of your next turn. After the Sword of Karstag’s Wrath ends, it cannot be used until after 1d6 days.",
+
