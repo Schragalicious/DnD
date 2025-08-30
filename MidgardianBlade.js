8b96b1b2551d0b4705005f8cbd9cb2d16367bbe7
Zachary Schrag <134228976+Schragalicious@users.noreply.github.com>
2025-08-30 11:21:46 -0400

    Update and rename Midgardian Blade to MidgardianBlade.js

---
+++ MidgardianBlade.js
@@ -0,0 +1,27 @@
+var iFileName = "The Midgardian Blade";
+
+WeaponsList["The Midgardian Blade"] = {
+name : "The Midgardian Blade",
+source : ["HB", 1],
+regExpSearch : /The Midgardian Blade/i,
+type : "Martial",
+ability : 2,
+abilitytodamage : true,
+damage : [2, 20, "piercing"],
+range : "Melee",
+description : "Finesse",
+tooltip : "The Midgardian Blade emits a radiant glow, shedding bright light in a 20-foot radius and dim light for an additional 20 feet when drawn. Divine Smite: Once per long rest, when the wielder hits a creature with an attack using the Holy Blade of Midgard, they can choose to channel divine energy into the strike, dealing an additional 4d8 radiant damage. The damage increases to 6d8 against undead and fiends. Divine Protection: Once per long rest, The Midgardian Blade can bestow divine protection onto the wielder. The wielder gains advantage on saving throws against spells and other magical effects for 1d6 rounds.",
+list : "melee",
+modifiers : [2,"2"],
+isMagicWeapon : true,
+}
+
+MagicItemsList["The Midgardian Blade"] = {
+name : "The Midgardian Blade",
+source : ["HB", 0],
+type : "weapon",
+rarity : "artifact",
+attunement : false,
+weight : 14,
+description : "The Midgardian Blade emits a radiant glow, shedding bright light in a 20-foot radius and dim light for an additional 20 feet when drawn. Divine Smite: Once per long rest, when the wielder hits a creature with an attack using the Holy Blade of Midgard, they can choose to channel divine energy into the strike, dealing an additional 4d8 radiant damage. The damage increases to 6d8 against undead and fiends. Divine Protection: Once per long rest, The Midgardian Blade can bestow divine protection onto the wielder. The wielder gains advantage on saving throws against spells and other magical effects for 1d6 rounds.",
+}
